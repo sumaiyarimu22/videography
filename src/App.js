a1a9c868001309ae66a44896475ef995724e4ac3
sumaiyarimu22 <sumaiyarimu22@gmail.com>
2024-01-05 00:07:57 +0600

    darkMood and lightMood add

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
+import styled, { ThemeProvider } from "styled-components";
+import { darkTheme, lightTheme } from "./utils/Themes";
+
+const Container = styled.div`
+  background: ${({ theme }) => theme.bg};
+  width: 100%;
+  height: 100vh;
+`;
 
 const App = () => {
-  return <div>App</div>;
+  const [darkMood, setDarkMood] = useState(false);
+  return (
+    <ThemeProvider theme={darkMood ? darkTheme : lightTheme}>
+      <Container>App</Container>
+    </ThemeProvider>
+  );
 };
 
 export default App;
